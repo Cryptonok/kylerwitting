11710a5a0cdeff9289e03917fea678353da31760
KylerWitting <kylerwitting@gmail.com>
2015-12-26 22:56:52 -0500

    Fixed the Buttons

--- js/custom.js
+++ js/custom.js
@@ -1,32 +1,43 @@
 // jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
+  if ($(".navbar").offset().top > 50) {
+    $(".navbar-fixed-top").addClass("top-nav-collapse");
+  } else {
+    $(".navbar-fixed-top").removeClass("top-nav-collapse");
+  }
 });
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
+  $('a.page-scroll').bind('click', function(event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: $($anchor.attr('href')).offset().top
+    }, 1500, 'easeInOutExpo');
+    event.preventDefault();
+  });
 });
 
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
+  $('.navbar-toggle:visible').click();
 });
 
 function gitHub() {
-    window.location.assign("https://github.com/KylerWitting")
+  window.location.assign("https://github.com/KylerWitting")
 }
 
 function faceBook() {
-    window.location.assign("https://www.facebook.com/kyler.witting")
+  window.location.assign("https://www.facebook.com/kyler.witting")
 }
+
+$(document).ready(function() {
+  $('#emailBtn').click(function() {
+    $('#fr1').attr('action',
+                   'mailto:kylerwitting@gmail.com?subject=';
+                   });
+  });
+
+  $( "#emailBtn" ).on( "click", function(event) {
+    $(this).attr('href', 'mailto:kylerwitting@gmail.com');
+  });
